e5cbea1cf80a71ee2609c19b350ed9311bcb4b0c
Bharat Arkasali <apradeep7@gmail.com>
2024-07-17 19:52:11 +0530

    added changes

--- src/components/maincontent/maincontent.jsx
+++ src/components/maincontent/maincontent.jsx
@@ -1,50 +1,84 @@
-import { useEffect, useState } from "react"
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Product from "../product/product";
 
 export default function MainContent(props) {
+  const [productList, setProductList] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState({});
 
-    const [productList, setProductList] = useState([]);
-
-    useEffect(() => {
-        axios.get(`${props.aipBaseUrl}/products`).then((response) => {
-            setProductList(response.data);
-        });
+  useEffect(() => {
+    axios.get(`${props.aipBaseUrl}/products/categories`).then((response) => {
+      setCategories(response.data);
     });
+  }, []);
 
-    return (<>
-        <section class="section" id="products">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-4">
-                        <div class="section-heading">
-                            <span><b>Shop by category: </b></span>
-                            <select class="form-select" aria-label="Default select example">
-                                <option selected>Open this select menu</option>
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-lg-8">
-                        <div class="section-heading">
-                            <h2>Our Latest Products</h2>
-                            <span>Check out all of our products.</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="container">
-                <div class="row">
-                    {
-                        productList.map((product) => (
-                            <Product productInfo={product} />
-                        ))
-                    }
+  useEffect(() => {
+    axios.get(`${props.aipBaseUrl}/products`).then((response) => {
+      setProductList(response.data);
+    });
+  }, []);
 
-                </div>
+  const onCategorySelected = (selCategory) => {
+    setSelectedCategory(selCategory);
+    getProductsByCategory(selCategory);
+  };
+
+  const getProductsByCategory = (cat) => {
+    if (!!cat) {
+      axios
+        .get(`${props.aipBaseUrl}/products/category/${cat}`)
+        .then((response) => {
+          setProductList(response.data);
+        });
+    } else {
+      axios.get(`${props.aipBaseUrl}/products`).then((response) => {
+        setProductList(response.data);
+      });
+    }
+  };
+
+  return (
+    <>
+      <section class="section" id="products">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4">
+              <div class="section-heading">
+                <span>
+                  <b>Shop by category: </b>
+                </span>
+                <select
+                  onChange={(e) => onCategorySelected(e.target.value)}
+                  value={selectedCategory}
+                  class="form-select"
+                  aria-label="Default select example"
+                >
+                  <option value="" selected>
+                    Open this select menu
+                  </option>
+                  {categories.map((cat) => (
+                    <option value={cat}>{cat}</option>
+                  ))}
+                </select>
+              </div>
             </div>
-        </section>
-    </>)
+            <div class="col-lg-8">
+              <div class="section-heading">
+                <h2>Our Latest Products</h2>
+                <span>Check out all of our products.</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            {productList.map((product) => (
+              <Product productInfo={product} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
--- src/components/productdetail/productdetail.jsx
+++ src/components/productdetail/productdetail.jsx
@@ -1,51 +1,67 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export default function ProductDetail(){
-    let {id} = useParams();
-    return(<>
-    <section class="section" id="product">
+export default function ProductDetail(props) {
+  let { id } = useParams();
+  const [productDetail, setProductDetail] = useState({});
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${props.aipBaseUrl}/products/${id}`).then((response) => {
+      setProductDetail(response.data);
+    });
+  });
+  return (
+    <>
+      <section style={{ margin: "140px 0 0 0" }} class="section" id="product">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-8">
-                <div class="left-images">
-                    <img src="assets/images/single-product-01.jpg" alt="" />                    
-                </div>
+          <div class="row">
+            <div class="col-lg-8">
+              <div class="left-images product-detail-image">
+                <img src={productDetail.image} alt="" />
+              </div>
             </div>
             <div class="col-lg-4">
-                <div class="right-content">
-                    <h4>New Green Jacket</h4>
-                    <span class="price">$75.00</span>
-                    <ul class="stars">
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                    </ul>
-                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod kon tempor incididunt ut labore.</span>
-                    <div class="quote">
-                        <i class="fa fa-quote-left"></i><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiuski smod.</p>
+              <div class="right-content">
+                <h4>{productDetail.title}</h4>
+                <span class="price">&#8377; {productDetail.price}</span>
+                <span>{productDetail.description}</span>
+                <div class="quantity-content">
+                  <div class="left-content">
+                    <h6>No. of Orders</h6>
+                  </div>
+                  <div class="right-content">
+                    <div class="quantity buttons_added">
+                      <input type="button" value="-" class="minus" />
+                      <input
+                        type="number"
+                        step="1"
+                        min="1"
+                        max=""
+                        name="quantity"
+                        value="1"
+                        title="Qty"
+                        class="input-text qty text"
+                        size="4"
+                        pattern=""
+                        inputmode=""
+                      />
+                      <input type="button" value="+" class="plus" />
                     </div>
-                    <div class="quantity-content">
-                        <div class="left-content">
-                            <h6>No. of Orders</h6>
-                        </div>
-                        <div class="right-content">
-                            <div class="quantity buttons_added">
-                                <input type="button" value="-" class="minus" />
-                                <input type="number" step="1" min="1" max="" name="quantity" value="1" title="Qty" class="input-text qty text" size="4" pattern="" inputmode="" />
-                                <input type="button" value="+" class="plus" />
-                            </div>
-                        </div>
-                    </div>
-                    <div class="total">
-                        <h4>Total: $210.00</h4>
-                        <div class="main-border-button"><a href="#">Add To Cart</a></div>
-                    </div>
+                  </div>
                 </div>
+                <div class="total">
+                  <h4>Total: &#8377;210.00</h4>
+                  <div class="main-border-button">
+                    <a href="#">Add To Cart</a>
+                  </div>
+                </div>
+              </div>
             </div>
-            </div>
+          </div>
         </div>
-    </section>
-    </>)
+      </section>
+    </>
+  );
 }
